e437573f8f8fdbae16e22b35370ff3197be93bd0
TylerNSmith8001 <tylernsmith8001@gmail.com>
2020-09-21 18:04:13 -0400

    I lied -the site name not linking to home, was bothering me. -it should be an image, but it wont be for a while.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Row, Col, Container } from "react-bootstrap"
-import {Link} from "react-router-dom"
+import {Link, useHistory} from "react-router-dom"
 import useRoommate from "../hooks/useRoommate"
 
 function Header() {
     //pull in user
     let {roommate} = useRoommate();
+
+    let history = useHistory()
 
     let hour = (new Date()).getHours();
 
@@ -13,10 +15,12 @@ function Header() {
         <Container className="p-2 pr-4 mx-0 mb-3 h3 border-bottom border-dark" style={{backgroundColor: "rgba(128,128,128,0.5)", boxShadow: "rgba(0,0,0,0.5) 0rem 0.5rem 2rem", maxWidth: "100vw", overflow: "hidden"}}>
             <Row>
                 <Col className="text-left">
-                    {/*Icon and they were... Roommates*/}
-                    <img src="/android-chrome-192x192.png" alt="Smiling Face with Smiling Eyes and Hand Covering Mouth" style={{height:"2rem", width:"2rem"}}/>
-                    <span className="pl-2 position-relative" style={{top: "0.4rem"}}>Roommates</span>
-                    <small className="pl-2 position-relative h6 text-dark" style={{top: "-1.15rem", left: "-10.25rem"}}>and they were...</small>
+                    <button type="button" class="btn btn-link" onClick={ () => history.push('/')}>
+                        {/*Icon and they were... Roommates*/}
+                        <img src="/android-chrome-192x192.png" alt="Smiling Face with Smiling Eyes and Hand Covering Mouth" style={{height:"2rem", width:"2rem"}}/>
+                        <span className="pl-2 h3 position-relative" style={{top: "0.4rem", color: "black"}}>Roommates</span>
+                        <small className="pl-2 position-relative h6 text-dark" style={{top: "-1.15rem", left: "-10.25rem"}}>and they were...</small>
+                    </button>
                 </Col>
                 <Col className="text-right">
                     <span className="h5 pr-3">
